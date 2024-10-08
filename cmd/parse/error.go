ff1698839aa0972f48e5bbb7afc5776d208a8720
imelon2 <imelon279@naver.com>
2024-10-08 18:38:35 +0900

    feat parse error

--- cmd/parse/error.go
+++ cmd/parse/error.go
@@ -8,39 +8,58 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	ethlib "github.com/imelon2/orbit-cli/ethLib"
 	"github.com/imelon2/orbit-cli/prompt"
+	"github.com/imelon2/orbit-cli/utils"
 	"github.com/spf13/cobra"
 )
+
+type ErrorDataLog struct {
+	System  string         `json:"system,omitempty"`
+	Message string         `json:"message,omitempty"` // 빈 값 허용
+	Custom  *CustomMessage `json:"custom,omitempty"`  // nil 값 허용
+	Hex     string         `json:"hex"`
+}
+
+type CustomMessage struct {
+	Name   string      `json:"name,omitempty"`
+	Params interface{} `json:"params,omitempty"`
+}
+
+// Revert reason ID for "Error(string)"
+var revertReasonID = "08c379a0"
 
 // errorCmd represents the error command
 var ErrorCmd = &cobra.Command{
 	Use:   "error",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("error called")
 
-		// abiPath := utils.GetAbiDir()
-		// _abi, err := os.ReadFile(abiPath)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		abiPath := utils.GetAbiDir()
+		_abi, err := os.ReadFile(abiPath)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		// abi, err := ethlib.GetAbi(strings.NewReader(string(_abi)))
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		abiParse, err := ethlib.GetAbi(strings.NewReader(string(_abi)))
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		providerOrError, isProvider, err := prompt.SelectProviderOrBytes()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		var data []byte
+		errRuslt := ErrorDataLog{}
+
 		if isProvider {
 			client, err := ethclient.Dial(providerOrError)
 			if err != nil {
@@ -58,6 +77,11 @@ var ErrorCmd = &cobra.Command{
 				log.Fatal(err)
 			}
 
+			if receipt.Status == 1 {
+				fmt.Printf("transaction hash %s is SUCCESS", receipt.TxHash)
+				return
+			}
+
 			callMsg := ethereum.CallMsg{
 				From:     *sender,
 				To:       tx.To(),
@@ -69,30 +93,114 @@ var ErrorCmd = &cobra.Command{
 				Value: tx.Value(),
 			}
 
-			bytes, errData := client.CallContract(context.Background(), callMsg, receipt.BlockNumber)
+			_, errData := client.CallContract(context.Background(), callMsg, receipt.BlockNumber)
+
 			rpcErr, ok := errData.(rpc.DataError)
 			if ok {
-				errorData := rpcErr.ErrorData()
-				errorMsg := rpcErr.Error()
-				// errorCode := rpcErr.ErrorCode()
-				fmt.Printf("errorMsg : %s\n", errorMsg)
-				fmt.Printf("errorData : %s\n", errorData)
-			}
-			if err != nil {
-				log.Fatal(err)
+				errRuslt.System = rpcErr.Error()
+				errRuslt.Hex, _ = rpcErr.ErrorData().(string)
+
+				if errRuslt.Hex == "" {
+					errRuslt.Hex = "0x"
+					errRuslt.Message = "NULL"
+				} else if errRuslt.Hex[2:10] == revertReasonID {
+					dataBytes, _ := hex.DecodeString(errRuslt.Hex[10:])
+
+					stringType, err := abi.NewType("string", "", nil)
+					arguments := abi.Arguments{
+						{Type: stringType},
+					}
+					decodeAbiString, err := arguments.Unpack(dataBytes)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					for _, s := range decodeAbiString {
+						_s, _ := s.(string)
+						errRuslt.Message = _s
+						break
+					}
+
+				} else {
+					// var revertMessage string
+					dataBytes, _ := hex.DecodeString(errRuslt.Hex[2:])
+					var sigdata [4]byte
+					for i, data := range dataBytes[:4] {
+						sigdata[i] = data
+					}
+
+					errorAbi, err := abiParse.ErrorByID(sigdata)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					decodedError, err := errorAbi.Inputs.Unpack(dataBytes[4:])
+					if err != nil {
+						log.Fatal(err)
+					}
+					jsonError := make(map[string]interface{})
+					for i, data := range decodedError {
+						jsonError[errorAbi.Inputs[i].Name] = utils.ConvertBytesToHex(data)
+					}
+
+					newCustom := CustomMessage{
+						Name:   errorAbi.Sig,
+						Params: jsonError,
+					}
+					errRuslt.Custom = &newCustom
+				}
 			}
 
-			data = bytes
 		} else {
-			providerOrError = providerOrError[2:]
-			ErrorBytes, err := hex.DecodeString(providerOrError)
-			if err != nil {
-				log.Fatalf("fail string calldata decode to hex: %v", err)
+
+			errRuslt.Hex = providerOrError
+			if errRuslt.Hex[2:10] == revertReasonID {
+				dataBytes, _ := hex.DecodeString(errRuslt.Hex[10:])
+
+				stringType, err := abi.NewType("string", "", nil)
+				arguments := abi.Arguments{
+					{Type: stringType},
+				}
+				decodeAbiString, err := arguments.Unpack(dataBytes)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				for _, s := range decodeAbiString {
+					_s, _ := s.(string)
+					errRuslt.Message = _s
+					break
+				}
+
+			} else {
+				// var revertMessage string
+				dataBytes, _ := hex.DecodeString(errRuslt.Hex[2:])
+				var sigdata [4]byte
+				for i, data := range dataBytes[:4] {
+					sigdata[i] = data
+				}
+				errorAbi, err := abiParse.ErrorByID(sigdata)
+				if err != nil {
+					log.Fatal(err)
+				}
+				decodedError, err := errorAbi.Inputs.Unpack(dataBytes[4:])
+				if err != nil {
+					log.Fatal(err)
+				}
+				jsonError := make(map[string]interface{})
+				for i, data := range decodedError {
+					jsonError[errorAbi.Inputs[i].Name] = utils.ConvertBytesToHex(data)
+				}
+
+				newCustom := CustomMessage{
+					Name:   errorAbi.Sig,
+					Params: jsonError,
+				}
+				errRuslt.Custom = &newCustom
 			}
-			data = ErrorBytes
 		}
 
-		fmt.Print(data)
+		utils.PrintPrettyJson(errRuslt)
 	},
 }
 
